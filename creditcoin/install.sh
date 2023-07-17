ec40ea20fbe24a0c9806a6a1492bb13d7fec93c3
Revonode <125292076+Revonode@users.noreply.github.com>
2023-07-18 00:40:20 +0700

    Add files via upload

---
+++ creditcoin/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# =======================================================
+#                   COPYRIGHT INFORMATION
+# =======================================================
+#
+# Author: Revonode
+#
+# All rights reserved. No part of this document may be reproduced, distributed, or transmitted in any form or by any means,
+# including photocopying, recording, or other electronic or mechanical methods, without the prior written permission of the author,
+# except in the case of brief quotations embodied in critical reviews and certain other noncommercial uses permitted by copyright law.
+#
+# =======================================================
+
+read -p "Masukkan nama validator: " validatorname
+read -p "Masukkan IP VPS: " myip
+
+docker run -d \
+ --name creditcoin-validator \
+ -p 30333:30333 \
+ -v airdropsultan:/creditcoin-node/data  \
+ gluwa/creditcoin:2.222.2-testnet \
+ --name "$validatorname" \
+ --telemetry-url "wss://telemetry.creditcoin.network/submit/ 0" \
+ --public-addr "/dns4/$myip/tcp/30333" \
+ --chain test \
+ --bootnodes "/dns4/testnet-bootnode.creditcoin.network/tcp/30333/p2p/12D3KooWG3eEuYxo37LvU1g6SSESu4i9TQ8FrZmJcjvdys7eA3cH" "/dns4/testnet-bootnode2.creditcoin.network/tcp/30333/p2p/12D3KooWLq7wCMQS3qVMCNJ2Zm6rYuYh74cM99i9Tm8PMdqJPDzb" "/dns4/testnet-bootnode3.creditcoin.network/tcp/30333/p2p/12D3KooWAKUrvmchoLomoouoN1sKfF9kq8dYtCVFvtPuvqp7wFBS" \
+ --validator \
+ --base-path /creditcoin-node/data \
+ --port 30333 \
+ >/dev/null 2>&1 &
+ 
+output1=$(docker exec -it creditcoin-validator creditcoin-cli new | grep -oP '(?<=Seed phrase: ).*'); echo $output1; address1=$(docker exec -i creditcoin-validator creditcoin-cli show-address <<< $output1 | awk '/Account address:/ {print $NF}'); echo $address1; output2=$(docker exec -it creditcoin-validator creditcoin-cli new | grep -oP '(?<=Seed phrase: ).*'); echo $output2; address2=$(docker exec -i creditcoin-validator creditcoin-cli show-address <<< $output2 | awk '/Account address:/ {print $NF}'); echo $address2;
+
+read -p "Silakan untuk mentransfer ke dua alamat di atas dengan CTC minimal 1500 token. Jika sudah ditransfer, ketik Y: " transfer_confirmation
+
+if [[ $transfer_confirmation == "Y" || $transfer_confirmation == "y" ]]; then
+  expect -c "
+    spawn docker exec -i creditcoin-validator creditcoin-cli wizard -a 1000
+    expect \"Input pertama:\"
+    send \"$output1\n\"
+    expect \"Input kedua:\"
+    send \"$output2\n\"
+    interact
+    wait
+  "
+  sleep 10  # Jeda waktu sebelum menjalankan perintah selanjutnya
+fi
+
+docker exec -it creditcoin-validator creditcoin-cli status -a $address1
---
+++ thepower/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo -e "\e[1m\e[32mUpdating and install packages... \e[0m" && sleep 1
+
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32minstall erlang and update dependencies... \e[0m" && sleep 2
+
+apt install cmake clang gcc git curl libssl-dev build-essential automake autoconf libncurses5-dev elixir erlang erlang-base erlang-public-key erlang-ssl -y
+
+echo -e "\e[1m\e[32mDownload and build the node... \e[0m" && sleep 1
+# Build node v0.14.8
+cd $HOME
+rm -rf tpnode
+git clone https://github.com/thepower/tpnode.git -b e24
+cd tpnode
+git checkout v0.14.8
+./rebar3 get-deps
+./rebar3 compile
+./rebar3 release
+cp -r _build/default/rel/thepower /opt
+
+echo -e "\e[1m\e[32mcreate service file... \e[0m" && sleep 1
+sudo tee /etc/systemd/system/powerd.service > /dev/null <<EOF
+[Unit]
+Description=powerd
+After=network.target
+[Service]
+Type=simple
+User=root
+Group=root
+ExecStart=/opt/thepower/bin/thepower foreground
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable powerd.service
+
+echo -e "\e[1m\e[32m============DONE==============... \e[0m" && sleep 1
